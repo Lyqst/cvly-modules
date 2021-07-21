cd488439c2355f750be1acc407b8f8c4e5c033db
Lyqst <78376862+Lyqst@users.noreply.github.com>
2021-07-21 17:01:10 -0400

    First two modules: 'ntrvlc' and 'stpr'

---
+++ src/plugin.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#include <rack.hpp>
+#include "components.hpp"
+
+using namespace rack;
+
+// Declare the Plugin, defined in plugin.cpp
+extern Plugin* pluginInstance;
+
+// Declare each Model, defined in each module source file
+// extern Model* modelMyModule;
+extern Model* modelNtrvlc;
+extern Model* modelStpr;
